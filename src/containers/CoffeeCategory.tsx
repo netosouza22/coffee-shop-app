f0eed1268f0d984f5efea2c1a37f4c07a2088608
Neto Sousa <netosouzacp@gmail.com>
2024-04-25 17:48:06 -0300

    feat: finish the structure of categories component

--- src/containers/CoffeeCategory.tsx
+++ src/containers/CoffeeCategory.tsx
@@ -1,52 +1,53 @@
-import { Pressable, SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native'
+import { Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { CoffeeType } from '../pages/HomeScreen'
 
-export const CoffeeCategory = () => {
+interface CoffeeCategoryProps {
+  coffeeTypeChoosed: CoffeeType
+  handleCoffeeType: (type: CoffeeType) => void
+}
+export const CoffeeCategory = ({ coffeeTypeChoosed, handleCoffeeType }: CoffeeCategoryProps) => {
   return (
     <SafeAreaView>
       <ScrollView
         horizontal
-        style={styles.container}
         scrollEnabled={true}
         showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.container}
       >
-        {/* <View style={styles.buttonContainer}> */}
-
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Hot</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
-        <Pressable style={styles.btn}>
-          <Text style={styles.textBtn}>Iced</Text>
-        </Pressable>
+        <View style={styles.pressableContainer}>
+          <Pressable
+            style={[
+              styles.btn,
+              coffeeTypeChoosed === 'hot' ? styles.selectedBtn : styles.unselectedBtn,
+            ]}
+            onPress={() => handleCoffeeType('hot')}
+          >
+            <Text
+              style={[
+                styles.textBtn,
+                coffeeTypeChoosed === 'hot' ? styles.textBtnSelected : styles.textBtnUnSelected,
+              ]}
+            >
+              Hot
+            </Text>
+          </Pressable>
+          <Pressable
+            style={[
+              styles.btn,
+              coffeeTypeChoosed === 'iced' ? styles.selectedBtn : styles.unselectedBtn,
+            ]}
+            onPress={() => handleCoffeeType('iced')}
+          >
+            <Text
+              style={[
+                styles.textBtn,
+                coffeeTypeChoosed === 'iced' ? styles.textBtnSelected : styles.textBtnUnSelected,
+              ]}
+            >
+              Iced
+            </Text>
+          </Pressable>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
@@ -54,12 +55,15 @@ export const CoffeeCategory = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  pressableContainer: {
+    flex: 1,
     flexDirection: 'row',
-    rowGap: 8,
+    justifyContent: 'center',
   },
   btn: {
-    flex: 1,
-    backgroundColor: '#C67C4E',
+    width: 'auto',
     borderRadius: 12,
     paddingHorizontal: 16,
     paddingVertical: 10,
@@ -70,5 +74,19 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: '600',
     fontSize: 14,
+  },
+  textBtnSelected: {
+    color: '#ffffff',
+  },
+  textBtnUnSelected: {
+    color: '#C67C4E',
+  },
+  selectedBtn: {
+    backgroundColor: '#C67C4E',
+    color: 'white',
+  },
+  unselectedBtn: {
+    backgroundColor: 'white',
+    color: '#C67C4E',
   },
 })
