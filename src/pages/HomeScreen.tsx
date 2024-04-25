bc2c0c024d01697c00fa67518572a62e64310f6a
Neto Sousa <netosouzacp@gmail.com>
2024-04-25 17:52:07 -0300

    feat: add loading and functions to deal with the category change

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,38 +1,49 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { Image, StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import { RootStackParamList } from '../../App'
 import { CoffeeCategory } from '../containers/CoffeeCategory'
 import { Header } from '../containers/Header'
 
 import { LinearGradient } from 'expo-linear-gradient'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import CoffeeDrinksContainer from '../containers/CoffeeDrinksContainer'
 import { Drink } from '../types/Drinks'
 
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+export type CoffeeType = 'hot' | 'iced'
 
 const HomeScreen = ({ navigation }: HomeScreenProps) => {
+  const [loading, setLoading] = useState<Boolean>(false)
   const [data, setData] = useState<Drink[]>([])
-  const getData = async () => {
-    const resp = await fetch('https://api.sampleapis.com/coffee/hot')
-    const data: Drink[] = await resp.json()
-
-    const responseUpdate = data.map((drink) => {
-      const randomPrice = Math.random() * 1.0 + 4.0 // Preço aleatório entre 3.00 e 7.00
-      const randomNote = Math.random() * 1.5 + 3.5 // Preço aleatório entre 3.00 e 5.00
-
-      return {
-        ...drink,
-        note: Number(randomNote.toFixed(1)),
-        price: Number(randomPrice.toFixed(2)),
-      }
-    })
-
-    setData(responseUpdate)
-  }
+  const [coffeeType, setCoffeeType] = useState<CoffeeType>('hot')
 
   useEffect(() => {
+    console.log('coffeeType', coffeeType)
+    const getData = async () => {
+      setLoading(true)
+      const resp = await fetch(`https://api.sampleapis.com/coffee/${coffeeType}`)
+      const data: Drink[] = await resp.json()
+
+      const responseUpdate = data.map((drink) => {
+        const randomPrice = Math.random() * 1.0 + 4.0 // Preço aleatório entre 3.00 e 7.00
+        const randomNote = Math.random() * 1.5 + 3.5 // Preço aleatório entre 3.00 e 5.00
+
+        return {
+          ...drink,
+          note: Number(randomNote.toFixed(1)),
+          price: Number(randomPrice.toFixed(2)),
+        }
+      })
+
+      setData(responseUpdate)
+      setLoading(false)
+    }
+
     getData()
+  }, [coffeeType])
+
+  const handleCoffeeType = useCallback((coffeeType: CoffeeType) => {
+    setCoffeeType(coffeeType)
   }, [])
 
   return (
@@ -44,8 +55,12 @@ const HomeScreen = ({ navigation }: HomeScreenProps) => {
         </View>
       </LinearGradient>
       <View style={styles.bodyContainer}>
-        <CoffeeCategory />
-        {data !== undefined && <CoffeeDrinksContainer drinks={data} />}
+        <CoffeeCategory coffeeTypeChoosed={coffeeType} handleCoffeeType={handleCoffeeType} />
+        {data === undefined && loading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <CoffeeDrinksContainer drinks={data} />
+        )}
       </View>
     </View>
   )
@@ -54,7 +69,7 @@ const HomeScreen = ({ navigation }: HomeScreenProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgba(249, 249, 249, 1.0)',
+    backgroundColor: '#F9F9F9',
   },
   headerContainer: {
     position: 'relative',
@@ -65,7 +80,7 @@ const styles = StyleSheet.create({
   imgBannerContainer: {
     position: 'absolute',
     minWidth: '100%',
-    top: 100,
+    bottom: -70,
     right: 0,
     left: 0,
     alignItems: 'center',
@@ -73,6 +88,8 @@ const styles = StyleSheet.create({
   imgBanner: {},
   bodyContainer: {
     paddingTop: 88,
+    marginTop: 24,
+    gap: 24,
   },
 })
 
