cd48a2b12dd5d9ce3cd921690d03b9120319a611
Neto Sousa <netosouzacp@gmail.com>
2024-06-04 23:02:33 -0300

    feat: add new prop to enable scroll only when is needed

--- src/containers/CoffeeDrinksContainer.tsx
+++ src/containers/CoffeeDrinksContainer.tsx
@@ -1,21 +1,28 @@
-import { FlatList, View } from 'react-native'
+import { Dimensions, FlatList, View } from 'react-native'
 import DrinkCard from '../components/DrinkCard'
 import { Drink } from '../types/Drinks'
 
+const { height } = Dimensions.get('window')
 interface CoffeeDrinksContainerProps {
   drinks: Drink[]
+  scrollEnabled: boolean
 }
 
-const CoffeeDrinksContainer = ({ drinks }: CoffeeDrinksContainerProps) => {
+const CoffeeDrinksContainer = ({ drinks, scrollEnabled }: CoffeeDrinksContainerProps) => {
   return (
     <View style={{ flexDirection: 'row', paddingHorizontal: 30 }}>
       <FlatList
+        style={{ flex: 1, maxHeight: height - 250 }}
         numColumns={2}
         contentContainerStyle={{ gap: 16 }}
         columnWrapperStyle={{ gap: 16 }}
         data={drinks}
         keyExtractor={(drink) => String(drink.id)}
         renderItem={({ item }) => <DrinkCard drink={item} />}
+        nestedScrollEnabled={true}
+        scrollEnabled={!scrollEnabled}
+        showsVerticalScrollIndicator={false}
+        onEndReachedThreshold={0}
       />
     </View>
   )
